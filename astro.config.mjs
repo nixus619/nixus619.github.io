551e418d7aad4141e37a65a882ec02fa660ce96b
Nick Cedor <nickdcedor@gmail.com>
2024-05-22 10:59:08 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://nixus619.github.io',
-    output: 'static',
-    experimental: { assets: true }
+    site: 'https://nixus619.github.io'
 });
